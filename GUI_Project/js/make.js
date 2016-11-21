d5ec9037ed28eb69a3c42d495535d3beb9a02667
Taylor Rydahl <taylorr7@vt.edu>
2016-11-21 14:26:22 -0500

    Worked on exporting a .json file

--- GUI_Project/js/make.js
+++ GUI_Project/js/make.js
@@ -10,11 +10,14 @@
   "assumes": "recursion",
   "dispModComp": true,
   "glob_exer_options": {},
-  
-  "title: <input type="text" value=\"" + book[title] + "\">";
 */
 
 var jsonFile = "https://taylorr7.github.io/interfaceApplication/GUI_Project/CS3.json";
+var textFile = null; // Temporary global variable used for download link.
+
+$(document).on('focus', '.datepicker', function() {
+  $(this).datepicker();
+});
 
 $(document).on('click', '#toggle', function() {
 	$('#options').toggle();
@@ -23,6 +26,14 @@ $(document).on('click', '#toggle', function() {
 	} else {
 		$('#toggle').html("Show Options");
 	}
+});
+
+$(document).on('click', '#download', function() {
+  let json = buildJSON();
+  let download = document.getElementById('downloadLink');
+  download.href = makeFile(json);
+  alert("Ready for Download!");
+  $('#downloadLink').toggle();
 });
 
 $(document).on( 'click', '.collapse li a', function() {
@@ -34,23 +45,34 @@ $(document).on( 'click', '.collapse li a', function() {
 	}
 });
 
-const encode = ( key, val, index) => {
-	if( key.includes("'") ) {
-		htmlKey = key.replace( "'", "&#39;");
-	} else {
-		htmlKey = key;
+const prepArray = ( inputHTML ) => {
+  inputHTML = inputHTML.replace(/&amp;/g, "&");
+  let HTMLArray = inputHTML.split("<"); 
+  return HTMLArray;
+}
+
+const pullValue = ( inputString ) => {
+  let stringStart = inputString.search("value=\"");
+  let stringEnd = inputString.search("\" type=");
+  let value = inputString.slice(stringStart + 7, stringEnd);
+  return value;
+}
+
+const makePair = ( key, value ) => {
+  if(value === "{}") {
+    return "\"" + key + "\": " + value + ",\n";
+  } else {
+    return "\"" + key + "\": \"" + value + "\",\n";
+  }
+}
+
+const makeFile = ( textArray ) => {
+	if(textFile != null) {
+		window.URL.revokeObjectURL(textFile);
 	}
-	if( typeof val === 'object' && val !== null ) {
-		var output = "";
-		output = output + "<li id='" + htmlKey + "'><a>" + key + "</a><ul class='sortable" + index + " contain'>";
-		for( var entry in val ) {
-			output = output + encode( entry, val[entry], index + 1 );
-		}
-		output = output + "</ul></li>";
-		return output;
-	} else {
-		return "<li id='" + htmlKey + "'>" + key + ": <input type='text' name='" + htmlKey + "' value='" + val + "'></li>";
-	}
+	const data = new Blob([textArray], {type: 'text/plain'});
+	textFile = window.URL.createObjectURL(data);
+	return textFile;
 }
 
 const chapter = ( key, val, index ) => {
@@ -63,27 +85,165 @@ const chapter = ( key, val, index ) => {
   output += "<li><a>" + key + "</a>";
   output += "<ul>";
   for( let entry in val ) {
-    //output += "<li>" + val + "</li>";
     const path = entry.search('/') + 1;
     const entryName = entry.substr(path);
-    //output += "<li>" + entryName + "<ul>";
     output += "<li><a>" + val[entry]['long_name'] + "</a>";
-    //if(val[entry]['sections'] != null) {
-      //alert(val[entry]['sections']);
-      for( let section in val[entry]['sections'] ) {
-       //alert(section);
-       output += "<ul><li><a> Sections: </a><ul>";
-       output += "<li>" + section + "</li>";
-       output += "</ul></li></ul></li>";
+    for( let section in val[entry]['sections'] ) {
+      if(section != "{}") {
+        output += "<ul>";
+        //output += "<li> Hard Deadline: <input type=\"text\" value=\"" + $.datepicker.formatDate('mm/dd/yy', new Date()) + "\" class=\"datepicker\"> <br>";
+        //output += "<li> Soft Deadline: <input type=\"text\" value=\"" + $.datepicker.formatDate('mm/dd/yy', new Date()) + "\" class=\"datepicker\"> <br>";
+        output += "<li><a> Sections: </a><ul>";
+        output += "<li><a>" + section + "</a><ul>";
+        for( let exercise in val[entry]['sections'][section] ) {
+          output += "<li><a>" + exercise + "</a><ul>";
+          if(val[entry]['sections'][section][exercise]['long_name'] != null) {
+            output += "<li>Long Name: <input type=\"text\" value=\"" + val[entry]['sections'][section][exercise]['long_name'] + "\" readonly></li>";
+            if(val[entry]['sections'][section][exercise]['required']) {
+              output += "<li>Required: <select> <option value=\"true\" selected> True </option> <option value=\"false\"> False </option> </select> </li>"
+            } else {
+              output += "<li>Required: <select> <option value=\"true\"> True </option> <option value=\"false\" selected> False </option> </select> </li>"
+            }
+            output += "<li>Points: <input type=\"text\" value=\"" + val[entry]['sections'][section][exercise]['points'] + "\"></li>";
+            output += "<li>Threshold: <input type=\"text\" value=\"" + val[entry]['sections'][section][exercise]['threshold'] + "\"></li>";
+          }
+          output += "</ul></li>";
+          
+        }
+        output += "</ul></li>";
+        output += "</ul></li></ul></li>"; 
       }
-    //output += "</ul></li>"; 
+    }
   }
   output += "</ul></li>"
   return output;
 }
 
+const buildJSON = ( ) => {
+  let json = "{\n";
+  let spacing = "  ";
+  let header = $('#header').html();
+ 
+  let headerArray = prepArray(header);
+
+  $('#downloadLink').attr('download', pullValue(headerArray[3]));
+  json += spacing + makePair("title", pullValue(headerArray[5]));
+  json += spacing + makePair("desc", pullValue(headerArray[7]));
+  
+  let options = $('#options').html();
+  let optionArray = prepArray(options);
+  json += spacing + makePair("build_dir", pullValue(optionArray[1]));
+  json += spacing + makePair("code_dir", pullValue(optionArray[3]));
+  json += spacing + makePair("lang", pullValue(optionArray[5]));
+  json += spacing + makePair("build_JSAV", pullValue(optionArray[7]));
+  json += spacing + makePair("build_cmap", pullValue(optionArray[9]));
+  json += spacing + makePair("suppress_todo", pullValue(optionArray[11]));
+  json += spacing + makePair("assumes", pullValue(optionArray[13]));
+  json += spacing + makePair("displayModComp", pullValue(optionArray[15]));
+  //json += spacing + makePair("glob_exer_options", pullValue(optionArray[17]));
+  json += spacing + makePair("glob_exer_options", "{}");
+  
+  let chapters = $('#chapters').html();
+  chapters = chapters.replace(/readonly=/g, "");
+  //chapters = chapters.replace( "readonly=\"\"/g", "");
+  let chapterArray = prepArray(chapters);
+  json += spacing + "\"chapters\": ";
+  for(i = 0; i < chapterArray.length; i++) {
+    //alert(chapterArray[i]);
+    let line = "";
+    if(chapterArray[i].startsWith("a")) {
+      let stringStart = chapterArray[i].search(">");
+      let stringEnd = chapterArray[i].search("$");
+      let value = chapterArray[i].slice(stringStart + 1, stringEnd);
+      line = spacing + "\"" + value + "\": ";
+    } else if(chapterArray[i].startsWith("li")) {
+      let stringStart = chapterArray[i].search("li>");
+      let stringEnd = chapterArray[i].search(":");
+      let value = chapterArray[i].slice(stringStart + 3, stringEnd);
+      if(value != "") {
+        line = spacing + "\"" + value + "\": ";
+      }
+    } else if(chapterArray[i].startsWith("/li")) { 
+      line = "\n";
+    } else if(chapterArray[i].startsWith("ul")) {
+      if(chapterArray[i+1].startsWith("/ul")) {
+				if((i + 3 < chapterArray.length) && (chapterArray[i+3].startsWith("li"))) {
+					line = "{},";
+					i++;
+				} else {
+					line = "{}";
+					i++;
+				}
+			} else {
+				line = "{ \n";
+				spacing = spacing + "  ";
+			}
+    } else if(chapterArray[i].startsWith("/ul")) {
+      spacing = spacing.slice(0, spacing.length-2);
+			if((i + 2 < chapterArray.length) && (chapterArray[i+2].startsWith("li"))) {
+				line = spacing + "},";
+			}
+			else {
+				line = spacing + "}";
+			}
+    } else if(chapterArray[i].startsWith("input")) {
+      let stringStart = chapterArray[i].search("value=\"");
+			let stringEnd = chapterArray[i].search("\" type=");
+      let value = chapterArray[i].slice(stringStart + 7, stringEnd);
+      if(value === "true" || value === "false") {
+        if((i + 2 < chapterArray.length) && (chapterArray[i+2].startsWith("li"))) {
+					line = value + ",";
+				}
+				else {
+					line = value;
+				}
+      } else if(!isNaN(parseFloat(value))) {
+        if((i + 2 < chapterArray.length) && (chapterArray[i+2].startsWith("li"))) {
+					line = value + ","
+				}
+				else {
+					line = parseFloat(value);
+				}
+      } else {
+        if((i + 2 < chapterArray.length) && (chapterArray[i+2].startsWith("li"))) {
+					line = "\"" + value + "\",";
+				}
+				else {
+					line = "\"" + value + "\"";
+				}
+      }
+    } else if(chapterArray[i].startsWith("option")) {
+      if(chapterArray[i].includes("selected")) {
+        let stringStart = chapterArray[i].search("value=\"");
+        let stringEnd = chapterArray[i].search("\" selected=");
+        let value = chapterArray[i].slice(stringStart + 7, stringEnd);
+        if(value === "true" || value === "false") {
+          if(value === "false" && (i + 4 < chapterArray.length) && (chapterArray[i+4].startsWith("li"))) {
+            line = value + ",";
+          } else if(value === "true" && (i + 6 < chapterArray.length) && (chapterArray[i+6].startsWith("li"))) {
+            line = value + ",";
+          }
+          else {
+            line = value;
+          }
+        }
+      }
+    } else {
+      //alert(chapterArray[i]);
+    }
+    json += line;
+  }
+  json += "\n}";
+  return json;
+}
+
+/*
+decode = function( ) {
+	file = file.replace(/ class="ui-sortable-handle"/g, "");
+}
+*/
+
 $.getJSON( jsonFile, function( data ) {
-	
 	let headerString = "<h1> Header: </h1>";
 	headerString += "File Name: <input type=\"text\" value=\"CS3.json\"> <br>";
 	headerString += "Title: <input type=\"text\" value=\"" + data['title'] + "\"> <br>";
@@ -104,16 +264,8 @@ $.getJSON( jsonFile, function( data ) {
 	
 	let chapterString = "<h1> Chapters: </h1> <ul class=\"collapse\">";
 	$.each( data['chapters'], function( key, val ) {
-		chapterString += encode( key, val, 1 );
+		chapterString += chapter( key, val, 1 );
 	});
 	chapterString += "</ul>";
 	$('#chapters').html(chapterString);
-	
 });
-
-/*
-	JSON => Chapters => Chapter Headings => 'long_name', 'sections' => Sections => Exercises
-	A chapter can have any number of headings, each of which has to have a 'long_name' and 'sections'.
-	Each section within 'sections' can have any number of exercises which each have four properties:
-	'long_name', 'required', 'points', 'threshold'.
-*/
