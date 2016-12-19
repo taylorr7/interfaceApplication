10ac76c886e035cbe8400bb91bbdee6a03549337
Taylor Rydahl <taylorr7@vt.edu>
2016-12-19 13:34:56 -0500

    Finished refactoring with mustache.

--- js/refactor.js
+++ js/refactor.js
@@ -15,6 +15,18 @@ $(document).on('focus', '.datepicker', function() {
   $(this).datepicker();
 });
 
+
+/*
+ * Sets the value attribute of an input tag when the user changes it.
+ */
+$(document).on('blur', ':input', function() {
+	$(this).attr('value', $(this).val());
+});
+
+$(document).on('blur', 'select', function() {
+	$(this).attr('data-key', $(this).val());
+});
+
 /*
  * The click event for the 'Save Book' button.
  * Currently, this saves the book as an html download object.
@@ -43,119 +55,102 @@ const makeFile = ( textArray ) => {
  * Function to build a json file from the html on the page.
  */
 const buildJSON = ( ) => {
-  /*
-  let json = "{\n";
-  let spacing = "  ";
-
-  let header = $('#heading').html();
-  let headerArray = prepArray(header);
-  json += decode( headerArray, false );
-
-  let options = $('#options').html();
-  let optionArray = prepArray(options);
-  json += decode( optionArray, false );
-
-  let chapters = $('#chapters').html();
-  chapters = chapters.replace(/readonly=/g, "");
-  let chapterArray = prepArray(chapters);
-  json += spacing + "\"chapters\": ";
-  json += decode( chapterArray );
-  
-  json += "\n}";
-  return json;
-  */
-	
-	let json = "{\n}";
-}
-
-const decode = ( fileArray ) => {
+	let json = "{\n";
+	let chapters = $('#content').html();
+	let chapterArray = prepArray(chapters);
+	json += decode( chapterArray );
+	json += "\n}";
+	return json;
 }
 
 /*
-const decode = ( fileArray, chapter = true ) => {
+ * Function to remove class declarations and ampersands from a given html string before
+ * turning it into an array, splitting on the '<' character.
+ */
+const prepArray = (inputHTML) => {
+  inputHTML = inputHTML.replace(/&amp;/g, "&");
+  inputHTML = inputHTML.replace(/ style="[^"]+"/g, "");
+  inputHTML = inputHTML.replace(/ style=""/g, "");
+  inputHTML = inputHTML.replace(/ class="[^"]+"/g, "");
+  inputHTML = inputHTML.replace(/ class=""/g, "");
+  inputHTML = inputHTML.replace(/ type="[^"]+"/g, "");
+  inputHTML = inputHTML.replace(/ type=""/g, "");
+  inputHTML = inputHTML.replace(/ readonly=/g, "");
+  let HTMLArray = inputHTML.split("<");
+  return HTMLArray;
+}
+
+const pullData = ( dataString ) => {
+  let value = "";
+  if (dataString.includes("data-key")) {
+    let stringStart = dataString.search("data-key=\"");
+    let stringEnd = dataString.search("\">");
+    value = dataString.slice(stringStart + 10, stringEnd);
+  }
+  return value;
+}
+
+const pullValue = ( dataString ) => {
+  let value = "";
+  if (dataString.includes("value")) {
+    let stringStart = dataString.search("value=\"");
+    let stringEnd = dataString.search("\">");
+    value = dataString.slice(stringStart + 7, stringEnd);
+  }
+  return value;
+}
+
+const decode = ( fileArray ) => {
 	let jsonString = "";
 	let spacing = "  ";
 	for(i = 0; i < fileArray.length; i++)
 	{
 		let line = "";
-		if(fileArray[i].startsWith("li")) {
-			let stringStart = fileArray[i].search("id=\"");
-			let stringEnd = fileArray[i].search("\">");
-			let value = fileArray[i].slice(stringStart + 4, stringEnd);
+		if(fileArray[i].startsWith("a")) {
+			let value = pullData(fileArray[i]);
 			line = spacing + "\"" + value + "\": ";
-		} else if(fileArray[i].startsWith("input")) {
-			let stringStart = fileArray[i].search("id=\"");
-			let stringEnd = fileArray[i].search("\" type=");
-			let id = "#" + fileArray[i].slice(stringStart + 4, stringEnd);
-      let value = $(id).val();
-			if(value === "true" || value === "false") {
-				if((i + 2 < fileArray.length) && (fileArray[i+2].startsWith("li"))) {
-					line = value + ",";
-				}
-				else {
-					line = value;
-				}
-			} else if(!isNaN(parseFloat(value))) {
-				if(!value.includes("/")) {
-					if((i + 2 < fileArray.length) && (fileArray[i+2].startsWith("li"))) {
-						line = parseFloat(value) + ","
-					}
-					else {
-						line = parseFloat(value);
-					}
-				} else {
-					line = "\"" + value + "\",";
-          // line = value + ",";
-				}
+		} else if (fileArray[i].startsWith("input")) {
+			let value = pullValue(fileArray[i]);
+			if (value === "true" || value === "false" || (!isNaN(parseFloat(value)) && !(value.includes("-")) && !(value.includes("/")))) {
+				line = value;
 			} else {
-				if((!chapter) || (i + 2 < fileArray.length) && (fileArray[i+2].startsWith("li"))) {
-					line = "\"" + value + "\",";
-				}
-				else {
-					line = "\"" + value + "\"";
-				}
+				line = "\"" + value + "\"";
 			}
-		} else if(fileArray[i].startsWith("ul")) {
-			if(fileArray[i+1].startsWith("/ul")) {
-				if((!chapter) || (i + 3 < fileArray.length) && (fileArray[i+3].startsWith("li"))) {
-					line = "{},";
-					i++;
-				} else {
-					line = "{}";
-					i++;
+			if ((i + 2 < fileArray.length) && (fileArray[i + 2].startsWith("li"))) {
+				line = line + ",";
+			}
+		} else if(fileArray[i].startsWith("select")) {
+			let value = pullData(fileArray[i]);
+			line = "\"" + value + "\",";
+		} else if (fileArray[i].startsWith("ul")) {
+			if (fileArray[i + 1].startsWith("/ul")) {
+				line = "{}";
+				if((i + 3 < fileArray.length) && (fileArray[i + 3].startsWith("li"))) {
+					line += ",";
 				}
+				i++;
 			} else {
-				if(chapter || (i != 1 && i != (fileArray.length - 1))) {
+				if(i != 1 && i != (fileArray.length - 1)) {
 					line = "{ \n";
 					spacing = spacing + "  ";
 				}
 			}
-		} else if(fileArray[i].startsWith("/li")) {
-			line = "\n";
-		} else if(fileArray[i].startsWith("/ul")) {
-			if(chapter || (i != 1 && i != (fileArray.length - 1))) {
-				spacing = spacing.slice(0, spacing.length-2);
-        if(chapter) {
-          if((i + 2 < fileArray.length) && (fileArray[i+2].startsWith("li"))) {
-            line = spacing + "},";
-          }
-          else {
-            line = spacing + "}";
-          }
-        } else {
-          line = spacing + "},";
-        }
+		} else if (fileArray[i].startsWith("/li")) {
+      		line = "\n";
+    	} else if (fileArray[i].startsWith("/ul")) {
+		  if (i != 1 && i != (fileArray.length - 1)) {
+			spacing = spacing.slice(0, spacing.length - 2);
+			if ((i + 2 < fileArray.length) && (fileArray[i + 2].startsWith("li"))) {
+				line = spacing + "},";
+			} else {
+				line = spacing + "}";
 			}
-		} else if(fileArray[i].startsWith("a")) {
-		} else if(fileArray[i].startsWith("/a")) {
-		} else {
-			//alert(fileArray[i]);
+		  }
 		}
 		jsonString += line;
 	}
 	return jsonString;
 }
-*/
 
 $(document).on('click', '.chapterLoad', function() {
 	$('#chapterSoft').attr('data-chapter', $(this).attr('data-chapter'));
@@ -194,9 +189,9 @@ $(document).ready(() => {
 		Handlebars.registerHelper('valCheck', function(key, value) {
 			if(key == "required") {
 				if(value == "true") {
-					return new Handlebars.SafeString("<select><option value=\"true\">true</option><option value=\"false\">false</option></select>");
+					return new Handlebars.SafeString("<select data-key=\"" + value + "\"><option value=\"true\">true</option><option value=\"false\">false</option></select>");
 				} else {
-					return new Handlebars.SafeString("<select><option value=\"true\">true</option><option value=\"false\">false</option></select>");
+					return new Handlebars.SafeString("<select data-key=\"" + value + "\"><option value=\"true\">true</option><option value=\"false\">false</option></select>");
 				}
 			} else if(typeof(value) === 'object') {
 				return new Handlebars.SafeString("<input value=\"{}\">");
